e9419a9efc4cc4ebaab40c70826585df8876136a
tesshin fukuoka <202311ap01_fukuoka@x-ide.sakura.ne.jp>
2024-03-11 16:29:37 +0900

    ユーザー情報　バグ修正

--- SRC/js/adminUser.js
+++ SRC/js/adminUser.js
@@ -22,13 +22,12 @@ function fnAdminUserEditCheck() {
 		return;
 	}
 
-	check = form.userNo.value;
 	tmp = form.password.value;
-	if (check.length == 0 && tmp.length == 0) {
+	if (tmp.length == 0) {
 		alert('PASSを入力してください');
 		return;
 	}
-	if (!tmp.length == 0 && tmp.length < 4 || tmp.length > 16 || tmp.match(/[^0-9a-zA-Z]+/)) {
+	if (tmp.length < 4 || tmp.length > 16 || tmp.match(/[^0-9a-zA-Z]+/)) {
 		alert('PASSは4桁以上16桁以下の半角英数字で入力してください');
 		return;
 	}
@@ -37,14 +36,14 @@ function fnAdminUserEditCheck() {
 		form.act.value = 'adminUserEditComplete';
 		form.submit();
 	}
+}
 
 
 
-	function fnAdminUserDeleteCheck(no, nm) {
-		if (confirm('「' + nm + '」を削除します。よろしいですか？')) {
-			form.userNo.value = no;
-			form.act.value = 'adminUserDelete';
-			form.submit();
-		}
+function fnAdminUserDeleteCheck(no, nm) {
+	if (confirm('「' + nm + '」を削除します。よろしいですか？')) {
+		form.userNo.value = no;
+		form.act.value = 'adminUserDelete';
+		form.submit();
 	}
 }
